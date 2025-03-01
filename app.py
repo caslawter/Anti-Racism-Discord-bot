d8c08c457a0bec56ed88ce975a94806c025a4591
caslawter <caslawter@gmail.com>
2025-03-01 21:35:50 +0800

    Updated return data sturcutre

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ toxigen_hatebert = pipeline("text-classification", model="tomh/toxigen_hatebert"
 def is_racist(sentence):
     output = toxigen_hatebert(sentence)
     print(output)
-    return output[0]['label'] == 'LABEL_1'
+    return output[0]['label'] == 'LABEL_1', output[0]['score']
 
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     CORS(app)
 
     # a simple page that says hello
@@ -32,7 +32,7 @@ def create_app(test_config=None):
             payload = request.get_json()
             sentence = payload['sentence']
             result = is_racist(sentence)
-            return jsonify({"data": result})
+            return jsonify({"is_racist": result[0], "score" : result[1]})
         except Exception as e:
             return jsonify({"error": str(e)})
 
